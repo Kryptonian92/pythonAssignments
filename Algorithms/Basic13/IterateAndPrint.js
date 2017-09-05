9346ea6ad9a5e7737b69409d3cea91641912c6ef
Ausar McGruder <ausar_mcgruder@yahoo.com>
2017-09-05 10:12:30 -0400

    9-5-17 Algorithms

---
+++ Algorithms/Basic13/IterateAndPrint.js
@@ -0,0 +1,5 @@
+// The following algorithm will print all values in a given array
+function printValues(arr){
+  for(var i = 0; i < arr.length; i++)
+  console.log("array[" + i + "] is equal to" + arr[i])
+}
